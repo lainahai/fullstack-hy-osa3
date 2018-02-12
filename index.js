b815b37996cb4ecb5dd24d24f7c4b59a4c919213
lainahai <jani.takkinen@cs.helsinki.fi>
2018-02-12 18:19:46 +0200

    3.4, 3.5

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+const getId = () => {
+  return Math.floor(Math.random() * 10000 + 1)
+}
 
 let persons = [
     {
@@ -45,6 +52,31 @@ app.get('/api/persons/:id', (request, response) => {
     response.status(404).end()
   }
 })
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  console.log("Deleting person with id", id)
+  persons = persons.filter(person => person.id != id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const person = request.body
+
+  let id = getId()
+  while(persons.find(person => person.id === id)){
+    id = getId()
+  }
+
+  person.id = id
+
+  console.log("Added new person ", person)
+
+  persons = persons.concat(person)
+  response.json(person)
+})
+
 
 const PORT = 3001
 app.listen(PORT, () => {
